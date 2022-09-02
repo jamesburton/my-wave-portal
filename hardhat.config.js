82fdcdc2d8dff6d79b6c83a60041c89fda4e642f
James Burton <james@code-consultants.co.uk>
2022-09-02 20:30:04 +0100

    Updated deployment, re-added example deploy as deploy-Lock, added dotenv and .env.example, and added testnet and mainnet config to hardhat.config.js

--- hardhat.config.js
+++ hardhat.config.js
@@ -10,7 +10,32 @@ task("accounts", "Prints the list of accounts", async () => {
   }
 });
 
+if(process.env.NODE_ENV === 'production') {
+  if(!process.env.PROD_QUICKNODE_URL?.length)
+    throw new Error('PROD_QUICKNODE_URL environment variable is missing');
+  if(!process.env.PRIVATE_KEY?.length)
+    throw new Error('PRIVATE_KEY environment variable is missing');
+} else {
+  if(!process.env.STAGING_QUICKNODE_URL?.length)
+    throw new Error('STAGING_QUICKNODE_URL environment variable is missing');
+  if(!process.env.STAGING_PRIVATE_KEY?.length)
+    throw new Error('STAGING_PRIVATE_KEY environment variable is missing');
+}
+
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.9",
+  // Configure networks (such as "rinkeby" TestNet)
+  networks: {
+    rinkeby: {
+      // This value will be replaced on runtime
+      url: process.env.STAGING_QUICKNODE_URL,
+      accounts: [process.env.STAGING_PRIVATE_KEY],
+    },
+    mainnet: {
+      chainId: 1,
+      url: process.env.PROD_QUICKNODE_URL,
+      accounts: [process.env.PRIVATE_KEY],
+    },
+  },
 };
