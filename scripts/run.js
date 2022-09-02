37f6d459905ba43a7aa6380610e56fc830a3e023
James Burton <james@code-consultants.co.uk>
2022-09-02 18:31:04 +0100

    Added initial waving

--- scripts/run.js
+++ scripts/run.js
@@ -1,8 +1,24 @@
 const main = async () => {
+    // const waveContractFactory = await hre.ethers.getContractFactory("WavePortal");
+    // const waveContract = await waveContractFactory.deploy();
+    // await waveContract.deployed();
+    // console.log("Contract deployed to:", waveContract.address);
+
+    const [owner, randomPerson] = await hre.ethers.getSigners();
     const waveContractFactory = await hre.ethers.getContractFactory("WavePortal");
     const waveContract = await waveContractFactory.deploy();
     await waveContract.deployed();
+
     console.log("Contract deployed to:", waveContract.address);
+    console.log("Contract deployed by:", owner.address);
+
+    let waveCount;
+    waveCount = await waveContract.getTotalWaves();
+
+    let waveTxn = await waveContract.wave();
+    await waveTxn.wait();
+
+    waveCount = await waveContract.getTotalWaves();
   };
   
   const runMain = async () => {
