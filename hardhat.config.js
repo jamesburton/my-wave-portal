1343e1fd35d4dd5f88ce6bd8f061d98322f588d1
James Burton <james@code-consultants.co.uk>
2022-09-03 11:59:45 +0100

    Configured to work with Rinkeby TestNet (and default to localhost)

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,3 +1,4 @@
+require("dotenv").config();
 require("@nomicfoundation/hardhat-toolbox");
 require("@nomicfoundation/hardhat-chai-matchers");
 require("@nomiclabs/hardhat-ethers");
@@ -10,6 +11,13 @@ task("accounts", "Prints the list of accounts", async () => {
   }
 });
 
+// Display config if running (HARDHAT_NETWORK is missing on compile)
+if(process.env.HARDHAT_NETWORK) {
+  console.log('nodenv=', process.env.NODE_ENV ?? typeof process.env.NODE_ENV);
+  // console.log(JSON.stringify(process.env));
+  console.log('HARDHAT_NETWORK=', process.env.HARDHAT_NETWORK ?? typeof process.env.HARDHAT_NETWORK);
+}
+
 if(process.env.NODE_ENV === 'production') {
   if(!process.env.PROD_QUICKNODE_URL?.length)
     throw new Error('PROD_QUICKNODE_URL environment variable is missing');
@@ -17,25 +25,45 @@ if(process.env.NODE_ENV === 'production') {
     throw new Error('PRIVATE_KEY environment variable is missing');
 } else {
   if(!process.env.STAGING_QUICKNODE_URL?.length)
-    throw new Error('STAGING_QUICKNODE_URL environment variable is missing');
+    throw new Error(`STAGING_QUICKNODE_URL environment variable is missing (NODE_ENV=${process.env.NODE_ENV})`);
   if(!process.env.STAGING_PRIVATE_KEY?.length)
-    throw new Error('STAGING_PRIVATE_KEY environment variable is missing');
+    throw new Error(`STAGING_PRIVATE_KEY environment variable is missing (NODE_ENV=${process.env.NODE_ENV})`);
 }
+
+var networks = {};
+if(process.env.STAGING_QUICKNODE_URL?.length)
+  networks.rinkeby = {
+    chainId: 4,
+    // chainId: 1337, // See https://hardhat.org/hardhat-network/docs/metamask-issue
+    // This value will be replaced on runtime
+    url: process.env.STAGING_QUICKNODE_URL,
+    accounts: [process.env.STAGING_PRIVATE_KEY],
+  };
+if(process.env.PROD_QUICKNODE_URL?.length)
+  networks.mainnet = {
+    chainId: 1,
+    // chainId: 1337,
+    url: process.env.PROD_QUICKNODE_URL,
+    accounts: [process.env.PRIVATE_KEY],
+  };
+const defaultNetwork = 'localhost';
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.9",
   // Configure networks (such as "rinkeby" TestNet)
-  networks: {
-    rinkeby: {
-      // This value will be replaced on runtime
-      url: process.env.STAGING_QUICKNODE_URL,
-      accounts: [process.env.STAGING_PRIVATE_KEY],
-    },
-    mainnet: {
-      chainId: 1,
-      url: process.env.PROD_QUICKNODE_URL,
-      accounts: [process.env.PRIVATE_KEY],
-    },
-  },
+  // networks: {
+  //   rinkeby: process.env.STAGING_QUICKNODE_URL && {
+  //     // This value will be replaced on runtime
+  //     url: process.env.STAGING_QUICKNODE_URL,
+  //     accounts: [process.env.STAGING_PRIVATE_KEY],
+  //   },
+  //   mainnet: process.env.PROD_QUICKNODE_URL && {
+  //     chainId: 1,
+  //     url: process.env.PROD_QUICKNODE_URL,
+  //     accounts: [process.env.PRIVATE_KEY],
+  //   },
+  // },
+  networks,
+  defaultNetwork,
 };
